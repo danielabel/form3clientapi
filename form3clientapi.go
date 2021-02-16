055dbfa4ab140927172d127eb74168b78cd09463
Dan Abel <dan.abel@tes.com>
2021-02-16 17:57:33 +0000

    think about logging not printing

--- form3clientapi.go
+++ form3clientapi.go
@@ -7,13 +7,9 @@ import (
     "fmt"
     "github.com/google/uuid"
     "io/ioutil"
+    "log"
     "net/http"
 )
-
-type accountsPayload struct {
-    Data []account `json:"data"`
-}
-
 
 type attributes struct {
     Country string `json:"country"`
@@ -30,7 +26,9 @@ type payload struct {
     Data account `json:"data"`
 }
 
-
+type accountsPayload struct {
+    Data []account `json:"data"`
+}
 
 var ErrAccountDoesNotExist = errors.New("account does not exist")
 var ErrOperationFailed = errors.New("operation failed")
@@ -39,7 +37,7 @@ const baseURL = "http://localhost:8080/v1"
 
 func createAccount() (account, error) {
 
-    p:= payload{Data: account{
+    p := payload{Data: account{
         Type:           "accounts",
         Id:             uuid.New(),
         OrganisationId: uuid.New(),
@@ -48,7 +46,7 @@ func createAccount() (account, error) {
         }}}
 
     requestBody, err := json.Marshal(p); if err != nil {
-        fmt.Printf("err %v\n", err)
+        log.Printf("err %v\n", err)
         return account{}, errors.New("fail Marshal")
     }
 
@@ -56,18 +54,18 @@ func createAccount() (account, error) {
         "application/vnd.api+json",
         bytes.NewBuffer(requestBody))
     if err != nil {
-        fmt.Printf("err %v\n", err)
+        log.Printf("err %v\n", err)
         return account{}, errors.New("fail Marshal")
     }
     defer resp.Body.Close()
 
     if resp.StatusCode != http.StatusCreated {
-        fmt.Printf("non 201 response %v %v", resp.Status, resp.Body)
+        log.Printf("non 201 response %v %v", resp.Status, resp.Body)
         return account{}, errors.New("non 201 response")
     }
 
     body, err := ioutil.ReadAll(resp.Body); if err != nil {
-        fmt.Printf("err %v\n", err)
+        log.Printf("err %v\n", err)
         return account{}, errors.New("failed to read body")
     }
 
@@ -81,7 +79,7 @@ func createAccount() (account, error) {
 
 func fetchAccount(id uuid.UUID) (account, error) {
     resp, err := http.Get(baseURL + "/organisation/accounts/" + id.String()); if err != nil {
-        fmt.Printf("err %v\n", err)
+        log.Printf("err %v\n", err)
         return account{}, errors.New("empty name")
     }
     defer resp.Body.Close()
@@ -102,8 +100,9 @@ func fetchAccount(id uuid.UUID) (account, error) {
     return s.Data, nil
 }
 func countAccounts(pageSize int) (int, error) {
-    resp, err := http.Get(fmt.Sprintf("%s/organisation/accounts?page[size]=%d", baseURL, pageSize)); if err != nil {
-        fmt.Printf("err %v\n", err)
+    url := fmt.Sprintf("%s/organisation/accounts?page[size]=%d", baseURL, pageSize)
+    resp, err := http.Get(url); if err != nil {
+        log.Printf("err %v\n", err)
         return 0, ErrOperationFailed
     }
     defer resp.Body.Close()
@@ -124,12 +123,12 @@ func deleteAccount(id uuid.UUID) error {
     client := &http.Client{}
     req, err := http.NewRequest("DELETE", baseURL + "/organisation/accounts/" + id.String() + "?version=0", nil)
     if err != nil {
-        fmt.Printf("NewRequest err %v\n", err)
+        log.Printf("NewRequest err %v\n", err)
         return errors.New("NewRequest err")
     }
 
     resp, err := client.Do(req); if err != nil {
-        fmt.Printf("err %v\n", err)
+        log.Printf("err %v\n", err)
         return errors.New("empty name")
     }
     defer resp.Body.Close()
